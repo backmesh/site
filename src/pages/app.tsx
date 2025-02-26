f42766f7617d99e6b7cec22b8d64feb5daa30470
lfdepombo <2381303+depombo@users.noreply.github.com>
2025-02-26 15:02:49 -0500

    use assign

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -3,12 +3,8 @@ import React, { useEffect } from 'react';
 export default function AppRedirect(): React.ReactElement {
   useEffect(() => {
     const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:8000" : "https://app.backmesh.com";
-    
-    // Get the current query string
     const queryString = window.location.search;
-    
-    // Redirect to the destination URL with the query parameters
-    window.location.href = baseUrl + queryString;
+    window.location.assign(baseUrl + queryString);
   }, []);
   return null;
 }
